6d9574cc3b72d55b6ebc9f8943672671a66b650c
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2025-12-24 07:22:01 -0500

    Updated index.js
    I'm adding the code needed to get the project working.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  alert("Generating poem");
+}
+
+let poemFormElement = document.querySelector("#peom-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
