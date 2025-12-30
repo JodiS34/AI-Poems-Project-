92455c4bf69181947627ef44bd346cf694388420
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2025-12-30 15:51:16 -0500

    Updated index.js
    I edited the index.js for a cleaner finish. I still have to fix the broken code before submitting this final project.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayPoem(response); {
+function displayPoem(response) {
 
     console.log("poem generatated");
 
@@ -15,16 +15,16 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user=instruction");
   let apiKey = "353edb486cab43d0074ob78cfft5c809";
     let prompt = ' User instructions: Generate a French poem about ${instructionsInput.value}';
-    let context = 'You are a romantic Poem expert and love to write short peoms. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions.Do not include a title to the poem. 'Sign the poem with SheCodes' <strong> AI' inside a </strong > element at the end of the poem and NOT at the beginning.';
+    let context = 'You are a romantic Poem expert and love to write short peoms. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions.Do not include a title to the poem. 
+    Sign the poem with 'SheCodes  AI' inside a <strong> element at the end of the poem and NOT at the beginning </strong>.';
         letapiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
 
-    console.log("Generating poem");
-    console.log('Prompt: ${prompt}');
-    console.log('Context: ${context}');
-
-
-
-
+    let poemElement = document.querySelector("#poem");
+    poemElement.computedStyleMap.display = "block"; 
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = '<div class= "generating">Generating a French poem about ${instructionsInput.value}</div>';
+    
+   
     axios.get(apiURL).then(displayPoem);
   
 }
